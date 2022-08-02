721b95e52b2c0114a48157b9d4291ecdb16fbdc7
nguyenvanhung112 <vanhungns96@gmail.com>
2022-08-02 20:53:38 +0700

    nop bai tap

--- Module2/src/ss2_array_loop/bai_tap/Add.java
+++ Module2/src/ss2_array_loop/bai_tap/Add.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 
 public class Add {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập vào số muốn thêm");
+        System.out.print("Nhập vào số muốn thêm\n");
         int number = sc.nextInt();
-        System.out.print("Nhập ví trị muốn thêm vào");
-        int index = sc.nextInt();
-        for (int i = array.length-1; i <index ; i--) {
-                array[i] = array[i-1];
+        int index;
+        do {
+            System.out.print("Nhập ví trị muốn thêm vào\n");
+            index = sc.nextInt();
+            if (index < 0 || index > array.length) {
+                System.out.print("Vui lòng nhập lại vị trí\n");
+            }
+        } while (index < 0 || index > array.length);
+        for (int i = array.length - 1; i < index; i--) {
+            array[i] = array[i - 1];
         }
         array[index] = number;
         System.out.print(Arrays.toString(array));
--- Module2/src/ss2_array_loop/bai_tap/Delete.java
+++ Module2/src/ss2_array_loop/bai_tap/Delete.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 
 public class Delete {
     public static void main(String[] args) {
-        int[] array = {8,2,6,64,4,76,6,45,15,10};
+        int[] array = {8, 2, 6, 64, 4, 76, 6, 45, 15, 10};
         System.out.print(Arrays.toString(array));
         Scanner sc = new Scanner(System.in);
         System.out.print("\nNhập vào số muốn xóa\n");
         int number = sc.nextInt();
         int index = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != number){
+            if (array[i] != number) {
                 array[index] = array[i];
                 index++;
             }
-        }array[array.length-1] = 0;
+        }
+        array[array.length - 1] = 0;
         System.out.print("Mảng sau khi xóa phần tử là " + Arrays.toString(array));
     }
 }
