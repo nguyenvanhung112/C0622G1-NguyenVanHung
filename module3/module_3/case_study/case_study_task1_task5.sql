75647a54cf3554a3ff649154b992860599b66988
nguyenvanhung112 <vanhungns96@gmail.com>
2022-09-14 23:25:26 +0700

    nop bai

--- module3/module_3/case_study/case_study_task1_task5.sql
+++ module3/module_3/case_study/case_study_task1_task5.sql
@@ -6,3 +6,12 @@ FROM khach_hang
 INNER JOIN  hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 INNER JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach WHERE loai_khach.ten_loai_khach = "Diamond" 
 GROUP BY hop_dong.ma_khach_hang ORDER BY so_lan_dat_phong;
+SELECT khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc, (ifnull(dich_vu.chi_phi_thue,0)+ SUM(ifnull(hop_dong_chi_tiet.so_luong,0)*ifnull(dich_vu_di_kem.gia,0))) as tong_tien
+FROM khach_hang
+JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach 
+LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+LEFT JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+LEFT JOIN dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+GROUP BY hop_dong.ma_hop_dong
+ORDER BY khach_hang.ma_khach_hang;
