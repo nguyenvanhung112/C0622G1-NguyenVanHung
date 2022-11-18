c0e9ef72ba18c259a008fdab7883d4aaa791e399
nguyenvanhung112 <vanhungns96@gmail.com>
2022-11-18 16:18:52 +0700

    nop bai

--- module5/Module5/src/type_script/excercise/count_fibonacci.js
+++ module5/Module5/src/type_script/excercise/count_fibonacci.js
@@ -1,16 +1,17 @@
-var arrayFibonacci;
 var count = 0;
 var number1 = 0;
 var number2 = 1;
 var fibonacci;
 var n = 20;
+var arrayFibonacci = [];
 for (var i = 0; i < n; i++) {
     fibonacci = number1 + number2;
+    arrayFibonacci.push(fibonacci);
     number1 = number2;
     number2 = fibonacci;
-    arrayFibonacci.push(fibonacci);
 }
 for (var i = 0; i < arrayFibonacci.length; i++) {
     count += arrayFibonacci[i];
 }
 console.log(arrayFibonacci);
+console.log(count);
--- module5/Module5/src/type_script/excercise/count_fibonacci.ts
+++ module5/Module5/src/type_script/excercise/count_fibonacci.ts
@@ -1,17 +1,23 @@
-let arrayFibonacci: number[];
 let count: number = 0;
 
 let number1: number = 0;
 let number2: number = 1;
 let fibonacci: number;
+
 let n: number = 20;
+
+let arrayFibonacci: number[] = [];
+
 for (let i = 0; i < n; i++) {
     fibonacci = number1 + number2;
+    arrayFibonacci.push(fibonacci);
     number1 = number2;
     number2 = fibonacci;
-    arrayFibonacci.push(fibonacci);
 }
+
 for (let i = 0; i < arrayFibonacci.length; i++) {
     count += arrayFibonacci[i];
 }
+
 console.log(arrayFibonacci);
+console.log(count);
