5127ad95dc475a7287b97b1021266096a45496be
nguyenvanhung112 <vanhungns96@gmail.com>
2022-11-27 21:11:33 +0700

    nop bai

---
+++ module5/case_study_5/case-study/src/app/model/customer.ts
@@ -0,0 +1,12 @@
+export interface Customer {
+  id: number,
+  img:string,
+  name: string,
+  dateOfBirth: string,
+  gender: number,
+  idCard: string,
+  phone: string,
+  email: string,
+  address: string,
+  customerType: number
+}
---
+++ module5/case_study_5/case-study/src/app/model/facility.ts
@@ -0,0 +1,15 @@
+export interface Facility {
+  id: number,
+  img:string,
+  name: string,
+  area: number,
+  cost: number,
+  maxPeople: number,
+  standardRoom: string,
+  descriptionOtherConvenience: string,
+  poolArea: number,
+  numberFloors: number,
+  facilityFree: string,
+  rentType: number,
+  facilityType: number
+}
